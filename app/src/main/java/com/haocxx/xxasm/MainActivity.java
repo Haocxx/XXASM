f6f9c5d137bd19997156b22aa2abc131f5072aad
Haocxx <haocxx.zhang@qq.com>
2019-09-17 10:57:15 +0800

    update

--- app/src/main/java/com/haocxx/xxasm/MainActivity.java
+++ app/src/main/java/com/haocxx/xxasm/MainActivity.java
@@ -3,8 +3,14 @@ package com.haocxx.xxasm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG1 = "MainActivity";
+    private static final String TAG2 = "MainActivity";
+    static final String TAG3 = "MainActivity";
+    final String TAG4 = "MainActivity";
+
     private int a = 1;
     protected int b = 1;
 
@@ -13,13 +19,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         test();
+        test2();
         new A();
         new B();
         b = a;
     }
 
     private void test() {
+        Log.d("Haocxx", new Integer(3).toString());
+        Log.d("Haocxx", "test2");
+        Log.d("Haocxx", "test3");
+        Log.d("Haocxx", "test4");
+    }
 
+    void test2() {
+        Log.d("Haocxx", new Integer(3).toString());
+        Log.d("Haocxx", "test2");
+        Log.d("Haocxx", "test3");
+        Log.d("Haocxx", "test4");
     }
 
     private class A {
