2a8b0471eda7a9230ee1859acf92f199399ea50b
Haocxx <haocxx.zhang@qq.com>
2019-10-22 20:15:40 +0800

    update

---
+++ app/src/main/java/com/haocxx/xxasm/BaseActivity.java
@@ -0,0 +1,15 @@
+package com.haocxx.xxasm;
+
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by Haocxx
+ * on 2019-10-22
+ */
+public class BaseActivity extends AppCompatActivity {
+    private void sortChildDrawingOrder() {
+        Log.d("Fuck", "asdasdasd");
+    }
+}
--- app/src/main/java/com/haocxx/xxasm/MainActivity.java
+++ app/src/main/java/com/haocxx/xxasm/MainActivity.java
@@ -1,65 +1,51 @@
 package com.haocxx.xxasm;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
-//    public static final String TAG1 = "MainActivity";
-//    private static final String TAG2 = "MainActivity";
-//    static final String TAG3 = "MainActivity";
-//    final String TAG4 = "MainActivity";
+import java.lang.reflect.Method;
 
-    private int a = 1;
-//    protected int b = 1;
+public class MainActivity extends BaseActivity {
+
+     long a = System.currentTimeMillis();
+    protected int b = 1;
+
+//    private static Method mSortChildDrawingOrderMethod = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        test(a);
-//        test2(a);
-//        new A();
-//        new B();
-//        new Handler().post(() -> {
-//            test(a);
-//            test2(a);
-//        });
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                test(a, 1);
-                test2(a, 1);
+                a = b;
+//                test(a, 1);
+//                test2(a, 1);
 
             }
         };
         new Handler().post(runnable);
-//        b = a;
+//
+//        try {
+//            if (mSortChildDrawingOrderMethod == null) {
+//                mSortChildDrawingOrderMethod = BaseActivity.class.getDeclaredMethod("sortChildDrawingOrder");
+//                mSortChildDrawingOrderMethod.setAccessible(true);
+//            }
+//            mSortChildDrawingOrderMethod.invoke(this);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 
-    private static void test(int a, int b) {
-//        Log.d("Haocxx", new Integer(3).toString());
-//        Log.d("Haocxx", "test2");
-//        Log.d("Haocxx", "test3");
-//        Log.d("Haocxx", "test4");
-//        Log.d("Haocxx", String.valueOf(this.a));
-
-    }
-
-    private void test2(int a, int b) {
-//        Log.d("Haocxx", new Integer(3).toString());
-//        Log.d("Haocxx", "test2");
-//        Log.d("Haocxx", "test3");
-//        Log.d("Haocxx", "test4");
-    }
-
-//    private class A {
+//    private static void test(int a, int b) {
+//
 //
 //    }
 //
-//    private static class B {
+//    private void test2(int a, int b) {
 //
 //    }
+
 }
